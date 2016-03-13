57a58207de48f9343f776fc3df90142c1d5d39b2
Thiago Pisani <pisani.thiago@gmail.com>
2016-03-13 20:39:38 -0300

    Tweaks on app.json and wsgi

--- flowdemo/wsgi.py
+++ flowdemo/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flowdemo.settings.dev")
 
 application = get_wsgi_application()
+
+try:
+    from dj_static import Cling
+    application = Cling(application)
+except:
+    pass
